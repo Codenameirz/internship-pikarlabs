af74849497c7c93c4fb43cc4eb351094010bdbc2
Haris Purwanto <purwantoharis499@gmail.com>
2021-01-17 21:31:52 +0700

    Komponen untuk array request 1, 2, dan 3

---
+++ php-logic/answer/file-tambahan/function.php
@@ -0,0 +1,56 @@
+<?php
+
+    function showProfile($input, $get){
+        $index = null;
+
+        foreach($input as $key => $value){
+            if($value['id'] == $get){
+                $index = $key;
+            }
+        }
+        return $index;
+    }
+
+    function showTable($input){
+        foreach($input as $data){
+        echo "<tr>";
+        echo "<td>".$data['id']."</td>";
+        echo "<td>".$data['nama']."</td>";
+        echo "<td>".$data['asal']."</td>";
+        echo "<td><a href=?id=".$data['id'].">Profil</a></td>";
+        echo "<tr>";
+        }
+    }
+
+    function searchId($input, $id){
+        $filter = [];
+
+        if(empty(trim($id))){
+            $id = "";
+            showTable($input);
+            die;
+        }
+
+        if(!empty($id)){
+            echo"<p>Mencari siswa dengan ID \"".$id."\"</p>";
+        }
+
+        foreach($input as $key => $value){
+            if(strpos($value['id'], $id) !== false){
+                array_push($filter,$value);
+            }
+        }
+        
+        //Jika $filter tidak kosong, maka tampilkan tabel
+        if(!empty($filter)){
+            showTable($filter);
+        }
+        //Jika kosong tampilkan pesan data tidak ditemukan
+        else{
+            echo"Data tidak ditemukan";
+        }
+    }
+
+    
+
+?>
---
+++ php-logic/answer/file-tambahan/profile.php
@@ -0,0 +1,7 @@
+<h3>Profil siswa ID = <?= $input[$index]['id'] ?></h3>
+
+<li>ID : <?= $input[$index]['id'] ?></li>
+<li>Nama : <?= $input[$index]['nama'] ?></li>
+<li>Asal : <?= $input[$index]['asal'] ?></li>
+<br>
+<a href="?">| Kembali |</a>
